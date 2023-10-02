71984381460d1eab855ae5f7c3c601de3b50243a
terence <terenceshawn18@outlook.com>
2023-10-02 14:51:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+const multiply=num1*num2;
+
+
+const random=Math.floor(Math.random())+1;
+
+let num3=14
+let num4=5
+const mod = num3%num4
+console.log(mod)
+
+let max = Math.max(1,20,3,5)
